331b5c157d588f255450ae70989b81d1c9538466
Søren Jørgensen <skmj@hotmail.dk>
2021-04-07 01:27:12 +0200

    Collision stuff (Collision er ikke sat på noget endnu))

---
+++ Components/Collider.cs
@@ -0,0 +1,72 @@
+﻿using DesignPaterns;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DesignPaterns.ComponentPatern;
+using DesignPaterns.ObserverPattern;
+
+namespace DesignPatterns
+{
+    class Collider: Component
+    {
+        public bool CheckCollisionEvents { get; set; }
+
+        private GameEvent onCollisionEvent = new GameEvent("Collision");
+
+        private Vector2 size;
+        private Vector2 origin;
+
+        private Texture2D texture;
+
+        public Rectangle CollisionBox
+        {
+            get
+            {
+               return new Rectangle
+                    (
+                        (int)(GameObject.Transform.Position.X - origin.X),
+                        (int)(GameObject.Transform.Position.Y - origin.Y),
+
+                        (int)(size.X),
+                        (int)(size.Y)
+
+                    );
+            }
+        }
+
+        public Collider(SpriteRenderer spriteRenderer)
+        {
+            this.origin = spriteRenderer.Origin;
+            this.size = new Vector2(spriteRenderer.Sprite.Width, spriteRenderer.Sprite.Height);
+            texture = Game1.Instance.Content.Load<Texture2D>("CollisionBoxRed");
+        }
+
+        public void OnCollisionEnter(Collider other)
+        {
+            if (CheckCollisionEvents)
+            {
+                if (other != this)
+                {
+                    if (CollisionBox.Intersects(other.CollisionBox))
+                    {
+                        onCollisionEvent.Notify(other);
+                    }
+                }
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, CollisionBox, null, Color.Red, 0, origin, SpriteEffects.None, 0);
+        }
+
+        public override string ToString()
+        {
+            return "Collider";
+        }
+    }
+
+}
+
---
+++ ObserverPattern/IGameListner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPaterns.ObserverPattern
+{
+    public interface IGameListner
+    {
+        void Notify(GameEvent gameEvent, Component component);
+    }
+}
